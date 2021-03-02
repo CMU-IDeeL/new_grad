def7d3a613adfbf95fbb8ee170ac241002628280
Kinori Rosnow <kinori.sr@gmail.com>
2021-03-02 17:21:52 -0500

    points

--- autograder/runner.py
+++ autograder/runner.py
@@ -13,60 +13,59 @@ from test_loss import *
 from test_functional import *
 
 tests = [
-    # funcitonals will be 0.5
     {
         'name': '0.1 - Autograd Add Operation',
         'autolab': 'Autograd Add Operation',
         'handler': test_add_operation,
-        'value': 2,
+        'value': 4,
     },
     {
         'name': '0.2 - Autograd Backward',
         'autolab': 'Autograd Backward',
         'handler': test_backward,
-        'value': 2,
+        'value': 4,
     },
     {
         'name': '1.1 - Functional Backward - Multiply',
         'autolab': 'Functional Backward - Multiply',
         'handler': test_mul_backward,
-        'value': 0.5,
+        'value': 1,
     },
     {
         'name': '1.2 - Functional Backward - Subtraction',
         'autolab': 'Functional Backward - Subtraction',
         'handler': test_sub_backward,
-        'value': 0.5,
+        'value': 1,
     },
     {
         'name': '1.3 - Functional Backward - Matmul',
         'autolab': 'Functional Backward - Matmul',
         'handler': test_matmul_backward,
-        'value': 0.5,
+        'value': 1,
     },
     {
         'name': '1.4 - Functional Backward - Divide',
         'autolab': 'Functional Backward - Divide',
         'handler': test_div_backward,
-        'value': 0.5,
+        'value': 1,
     },
     {
         'name': '1.5 - Functional Backward - Log',
         'autolab': 'Functional Backward - Log',
         'handler': test_log_backward,
-        'value': 0.5,
+        'value': 1,
     },
     {
         'name': '1.6 - Functional Backward - Exp',
         'autolab': 'Functional Backward - Exp',
         'handler': test_exp_backward,
-        'value': 0.5,
+        'value': 1,
     },
     {
         'name': '1.7 - Functional Backward - Pow',
         'autolab': 'Functional Backward - Pow',
         'handler': test_pow_backward,
-        'value': 0.5,
+        'value': 1,
     },
     {
         'name': '2.1 - Linear (Autograd) Forward',
@@ -78,7 +77,7 @@ tests = [
         'name': '2.2 - Linear (Autograd) Backward',
         'autolab': 'Linear (Autograd) Backward',
         'handler': test_linear_layer_backward,
-        'value': 3,
+        'value': 4,
     },
     {
         'name': '2.3 - Linear + Skip Connection (Autograd) Forward',
@@ -90,7 +89,7 @@ tests = [
         'name': '2.4 - Linear + Skip Connection (Autograd) Backward',
         'autolab': 'Linear + Skip Connection (Autograd) Backward',
         'handler': test_linear_skip_backward,
-        'value': 1,
+        'value': 2,
     },
     {
         'name': '3.1 - Identity (Autograd) Forward',
@@ -102,7 +101,7 @@ tests = [
         'name': '3.2 - Identity (Autograd) Backward',
         'autolab': 'Identity (Autograd) Backward',
         'handler': test_identity_backward,
-        'value': 3,
+        'value': 4,
     },
     {
         'name': '3.3 - Sigmoid (Autograd) Forward',
@@ -114,7 +113,7 @@ tests = [
         'name': '3.4 - Sigmoid (Autograd) Backward',
         'autolab': 'Sigmoid (Autograd) Backward',
         'handler': test_sigmoid_backward,
-        'value': 3,
+        'value': 4,
     },
     {
         'name': '3.5 - Tanh (Autograd) Forward',
@@ -126,7 +125,7 @@ tests = [
         'name': '3.6 - Tanh (Autograd) Backward',
         'autolab': 'Tanh (Autograd) Backward',
         'handler': test_tanh_backward,
-        'value': 3,
+        'value': 4,
     },
     {
         'name': '3.7 - ReLU (Autograd) Forward',
@@ -138,7 +137,7 @@ tests = [
         'name': '3.8 - ReLU (Autograd) Backward',
         'autolab': 'ReLU (Autograd) Backward',
         'handler': test_relu_backward,
-        'value': 3,
+        'value': 4,
     },
     {
         'name': '4.1 - SoftmaxCrossEntropy Loss (Autograd) Forward',
